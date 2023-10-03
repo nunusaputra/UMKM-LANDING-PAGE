a8097615c50f6e3aad0e9e970841945b7598fbfc
Wisnu Saputra <2010631170126@student.unsika.ac.id>
2023-10-03 22:46:07 +0700

    Projet KKN

---
+++ script/index.js
@@ -0,0 +1,7 @@
+var typed = new  Typed(".multiple-text", {
+    strings: ["ENAK", "LEZAT", "MURAH", "HALAL", "BERGIZI"],
+    typeSpeed: 70,
+    backSpeed: 20,
+    backDelay: 1000,
+    loop: true
+})
